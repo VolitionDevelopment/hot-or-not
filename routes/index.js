35029a390012d7969f986878bb21462bc882fafd
Jackson Yeager <jacksonyeager@gmail.com>
2016-09-07 13:53:02 -0400

    Added voting

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var mongoURL = 'mongodb://localhost:27017/hot-or-not';
+var db;
 
-/* GET home page. */
+mongoClient.connect(mongoURL, function(error, database){
+  if(error){
+    console.log(error);
+  }else{
+    db = database;
+    console.log('Connected to Mongo Successfully!');
+  }
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.collection('images').find().toArray(function(error, photos){
+    var randomNum = Math.floor(Math.random() * photos.length);
+    var randomPhoto = photos[randomNum].image;
+
+    res.render('index', { imageToRender: randomPhoto });
+  });
+});
+
+router.post('/hotornot', function(req, res, next){
+  db.collection('votes').insertOne({
+    ip: req.ip,
+    vote: req.body.submit,
+    image: req.body.image
+  });
+
+  res.redirect('/')
 });
 
 module.exports = router;
