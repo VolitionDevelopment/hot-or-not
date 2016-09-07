fb6660f8b9d5fab7c01de97e9c7ce401d3e0b197
Jackson Yeager <jacksonyeager@gmail.com>
2016-09-07 15:55:28 -0400

    Added standings page

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,46 @@ mongoClient.connect(mongoURL, function(error, database){
 });
 
 router.get('/', function(req, res, next) {
-  db.collection('images').find().toArray(function(error, photos){
-    var randomNum = Math.floor(Math.random() * photos.length);
-    var randomPhoto = photos[randomNum].image;
+  db.collection('votes').find({
+    ip: req.ip
+  }).toArray(function(error, result){
+    var photosVoted = [];
+    if(error){
+      console.log("Error fetching data...");
+      console.log(error);
+    }else{
+      for(var i = 0; i < result.length; i++){
+        photosVoted.push(result[i].image);
+      }
+    }
 
-    res.render('index', { imageToRender: randomPhoto });
+    console.log(photosVoted);
+
+    db.collection('images').find({
+      image: {$nin: photosVoted}
+    }).toArray(function(error, photos){
+      if(photos.length == 0){
+        res.redirect('/standings')
+      }else{
+        // console.log(photos);
+        var randomNum = Math.floor(Math.random() * photos.length);
+        var randomPhoto = photos[randomNum].image;
+
+        res.render('index', { imageToRender: randomPhoto });
+      }
+    });
   });
+});
+
+router.get('/standings', function(req, res, next){
+  db.collection('images').find().toArray(function(error, allResults){
+    var standingsArray = [];
+    allResults.sort(function(a,b){
+      return (b.totalVotes - a.totalVotes);
+    });
+    res.render('standings', {theStandings: allResults});
+  });
+
 });
 
 router.post('/hotornot', function(req, res, next){
@@ -28,6 +62,27 @@ router.post('/hotornot', function(req, res, next){
     ip: req.ip,
     vote: req.body.submit,
     image: req.body.image
+  });
+
+  db.collection('images').find({image: req.body.image}).toArray(function(error, results){
+    var total;
+    if (isNaN(results[0].totalVotes)) {
+      total = 0;
+    } else {
+      total = results[0].totalVotes;
+    }
+
+    db.collection('images').updateOne(
+        {
+          image: req.body.image
+        },
+        {
+          $set: (req.body.submit === 'Hot') ? {"totalVotes": (total + 1)} : {"totalVotes": (total - 1)}
+        },
+        function(error, results){
+
+        }
+    )
   });
 
   res.redirect('/')
